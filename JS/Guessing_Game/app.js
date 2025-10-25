5feb336647a144f5d021873fcd531a6f82797c30
Sainath1583 <124860017+Sainath1583@users.noreply.github.com>
2025-10-25 11:00:16 +0530

    commit

--- JS/Guessing_Game/app.js
+++ JS/Guessing_Game/app.js
@@ -5,15 +5,17 @@ while(!maximum){
 
 const targetNum =  Math.floor(Math.random() * maximum) + 1;
 
-let guess = parseInt(prompt("Enter your 1st guess!!"));
+let guess = prompt("Enter your 1st guess!!");
 let attemts = 1;
 while(parseInt(guess) !== targetNum){
     if(guess === 'q') break;
     attemts++;
     if(guess > targetNum){
-        guess = parseInt(prompt("To high! ENter a new guess"));
-    }else{
+        guess = parseInt(prompt("To high! Enter a new guess"));
+    }else if(guess < targetNum){
         guess = parseInt(prompt("To low! ENter a new guess"));
+    }else{
+        guess = parseInt(prompt("Invalid Number, Enter a valid number or enter 'q' to quit "));
     }
 }
 if(guess === 'q'){
